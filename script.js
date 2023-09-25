922e7b7d04ac02de8050cea56cb0c33c8bf66daa
bekri <babro.koy.kari@gmail.com>
2023-09-25 21:22:14 +0100

    ddddddqddqs

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 "use strict";
+
+$(document).ready(function() {
+  $("#loginbutton").on("click", function() {
+    var password = $("#password").val();
+    loadPage(password);
+  });
+
+  $("#password").keypress(function(e) {
+    if (e.which == 13) {
+      var password = $("#password").val();
+      loadPage(password);
+    }
+  });
+
+  $("#password").focus();
+});
+
 function loadPage(pwd) {
-    var hash = sha256(pwd); // Assuming you have a SHA-256 hashing function available
-    var url = hash + "/index.html";
-    $.ajax({
-        url: url,
-        dataType: "html",
-        success: function (data) {
-            window.location = url;
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            parent.location.hash = hash;
-            $("#password").attr("placeholder", "wrong password");
-            $("#password").val("");
-        }
+  var url = sha256Async(pwd) + "/index.html";
+  $.ajax({
+    url: url,
+    dataType: "html"
+  })
+    .done(function(data) {
+      window.location.href = url;
+    })
+    .fail(function(xhr, ajaxOptions, thrownError) {
+      parent.location.hash = sha256Async(pwd);
+      $("#password").attr("placeholder", "wrong password").val("");
     });
 }
-$("#loginbutton").on("click", function () {
-    loadPage($("#password").val());
-});
-$("#password").keypress(function (e) {
-    if (e.which == 13) {
-        loadPage($("#password").val());
-    }
-});
-$("#password").focus();
+
+function sha256Async(pwd) {
+  return new Promise(function(resolve, reject) {
+    sha256(pwd, function(hash) {
+      resolve(hash);
+    });
+  });
+}
